160adfcdd2aaede8553af9112dd1d69e3415b06e
Vitor Paladini <vitor@paladini.in>
2019-04-28 10:52:11 -0300

    style: 💄 Remove default styles and improve code formatting

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,48 +7,21 @@
 
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
 
-import { rhythm } from "../utils/typography"
+const BioQueryWrapper = () => <StaticQuery query={bioQuery} render={Bio} />
 
-function Bio() {
+const Bio = data => {
+  const { author, social } = data.site.siteMetadata
   return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author, social } = data.site.siteMetadata
-        return (
-          <div
-            style={{
-              display: `flex`,
-              marginBottom: rhythm(2.5),
-            }}
-          >
-            {/* <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            /> */}
-            <p>
-              Written by <strong>{author}</strong> who lives in São
-              Gonçalo and works at Stone Pagamentos.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
-            </p>
-          </div>
-        )
-      }}
-    />
+    <div>
+      <p>
+        Written by <strong>{author}</strong> who lives in São
+        Gonçalo and works at Stone Pagamentos.
+        <a href={`https://twitter.com/${social.twitter}`}>
+          You should follow him on Twitter
+        </a>
+      </p>
+    </div>
   )
 }
 
@@ -72,4 +45,4 @@ const bioQuery = graphql`
   }
 `
 
-export default Bio
+export default BioQueryWrapper
